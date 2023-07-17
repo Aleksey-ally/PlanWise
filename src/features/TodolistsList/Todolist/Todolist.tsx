6aa6c55feb6d3050de4fc6056910b80a98514239
Aleksey <easy.mail.to.copy@gmail.com>
2023-07-17 23:41:49 +0300

    Added key \"entityStatus\" for todolists and typed their

--- src/features/TodolistsList/Todolist/Todolist.tsx
+++ src/features/TodolistsList/Todolist/Todolist.tsx
@@ -9,6 +9,7 @@ import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
 import {Delete} from '@mui/icons-material';
 import {useAppDispatch} from "../../../app/store";
+import { RequestStatusType } from '../../../app/appReducer'
 
 type PropsType = {
     id: string
@@ -22,7 +23,7 @@ type PropsType = {
     removeTodolist: (id: string) => void
     changeTodolistTitle: (id: string, newTitle: string) => void
     filter: FilterValuesType
-
+    entityStatus: RequestStatusType
 }
 
 export const Todolist = React.memo(function (props: PropsType) {
