5ffbbf34403fe0fe8d2a01b73a5b8b463d0ce366
Aleksey <easy.mail.to.copy@gmail.com>
2023-08-07 20:23:15 +0300

    Refactored thunk updateTask into tasks.reducer to rtk thunk syntax and wherever it is used, rewrote test for updateTask

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { UpdateDomainTaskModelType } from "features/TodolistsList/tasks.reducer";
 
 const settings = {
   withCredentials: true,
@@ -120,3 +121,5 @@ export type AddTaskArgType = {
   title: string;
   todolistId: string;
 };
+
+export type UpdateTaskArgType = { taskId: string; domainModel: UpdateDomainTaskModelType; todolistId: string };
