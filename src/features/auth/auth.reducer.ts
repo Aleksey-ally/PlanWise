99c449b175279e3b10f912bc53fca980808f9431
Aleksey <easy.mail.to.copy@gmail.com>
2023-08-13 21:04:28 +0300

    Added comments for util func into handle-server-app-error and refactored login into auth.reducer

--- src/features/auth/auth.reducer.ts
+++ src/features/auth/auth.reducer.ts
@@ -38,7 +38,8 @@ const login = createAsyncThunk<{ isLoggedIn: boolean }, LoginParamsType, { rejec
         dispatch(appActions.setAppStatus({ status: "succeeded" }));
         return { isLoggedIn: true };
       } else {
-        handleServerAppError(res.data, dispatch, false);
+        const isShowAppError = !res.data.fieldsErrors.length;
+        handleServerAppError(res.data, dispatch, isShowAppError);
         return rejectWithValue(res.data);
       }
     } catch (e) {
