64e31bd12d397f40ecfe7f7a911dcbc58b415db4
Aleksey <easy.mail.to.copy@gmail.com>
2023-08-29 22:45:24 +0300

    Refactored component task, deleted funcs "removeTask", "changeTaskStatus" and "changeTaskTitle" from components todolists-list  and todolist, added model style for components task

--- src/features/todolists-list/todolists-list.tsx
+++ src/features/todolists-list/todolists-list.tsx
@@ -1,103 +1,92 @@
-import React, { useCallback, useEffect } from "react";
-import { useSelector } from "react-redux";
-import { FilterValuesType, todolistsActions, todolistsThunks } from "features/todolists-list/todolists/model/todolists.reducer";
-import { tasksThunks } from "features/todolists-list/tasks/model/tasks.reducer";
-import { Grid, Paper } from "@mui/material";
-import { Todolist } from "./todolists/ui/Todolist/Todolist";
-import { Navigate } from "react-router-dom";
-import { useActions } from "common/hooks";
-import { selectIsLoggedIn } from "features/auth/model/auth.selectors";
-import { selectTasks } from "features/todolists-list/tasks/model/tasks.selectors";
-import { selectTodolists } from "features/todolists-list/todolists/model/todolists.selectors";
-import { TaskStatuses } from "common/enums";
-import { AddItemForm } from "common/enums/components";
+import React, {useCallback, useEffect} from "react";
+import {useSelector} from "react-redux";
+import {
+    FilterValuesType,
+    todolistsActions,
+    todolistsThunks
+} from "features/todolists-list/todolists/model/todolists.reducer";
+import {tasksThunks} from "features/todolists-list/tasks/model/tasks.reducer";
+import {Grid, Paper} from "@mui/material";
+import {Todolist} from "features/todolists-list/todolists/ui/todolist/todolist";
+import {Navigate} from "react-router-dom";
+import {useActions} from "common/hooks";
+import {selectIsLoggedIn} from "features/auth/model/auth.selectors";
+import {selectTasks} from "features/todolists-list/tasks/model/tasks.selectors";
+import {selectTodolists} from "features/todolists-list/todolists/model/todolists.selectors";
+import {AddItemForm} from "common/enums/components";
 
 export const TodolistsList = () => {
-  const todolists = useSelector(selectTodolists);
-  const tasks = useSelector(selectTasks);
-  const isLoggedIn = useSelector(selectIsLoggedIn);
+    const todolists = useSelector(selectTodolists);
+    const tasks = useSelector(selectTasks);
+    const isLoggedIn = useSelector(selectIsLoggedIn);
 
-  const {
-    removeTodolist: removeTodolistThunk,
-    addTodolist: addTodolistThunk,
-    fetchTodolists,
-    changeTodolistTitle: changeTodolistTitleThunk,
-  } = useActions(todolistsThunks);
+    const {
+        removeTodolist: removeTodolistThunk,
+        addTodolist: addTodolistThunk,
+        fetchTodolists,
+        changeTodolistTitle: changeTodolistTitleThunk,
+    } = useActions(todolistsThunks);
 
-  const { addTask: addTaskThunk, removeTask: removeTaskThunk, updateTask } = useActions(tasksThunks);
-  const { changeTodolistFilter } = useActions(todolistsActions);
+    const {addTask: addTaskThunk} = useActions(tasksThunks);
+    const {changeTodolistFilter} = useActions(todolistsActions);
 
-  useEffect(() => {
+    useEffect(() => {
+        if (!isLoggedIn) {
+            return;
+        }
+        fetchTodolists();
+    }, []);
+
+
+    const addTask = useCallback(function (title: string, todolistId: string) {
+        addTaskThunk({title, todolistId});
+    }, []);
+
+    const changeFilter = useCallback(function (filter: FilterValuesType, id: string) {
+        changeTodolistFilter({id, filter});
+    }, []);
+
+    const removeTodolist = useCallback(function (id: string) {
+        removeTodolistThunk(id);
+    }, []);
+
+    const changeTodolistTitle = useCallback(function (id: string, title: string) {
+        changeTodolistTitleThunk({id, title});
+    }, []);
+
+    const addTodolist = useCallback((title: string) => {
+        addTodolistThunk(title);
+    }, []);
+
     if (!isLoggedIn) {
-      return;
+        return <Navigate to={"/login"}/>;
     }
-    fetchTodolists();
-  }, []);
 
-  const removeTask = useCallback(function (taskId: string, todolistId: string) {
-    removeTaskThunk({ taskId, todolistId });
-  }, []);
-
-  const addTask = useCallback(function (title: string, todolistId: string) {
-    addTaskThunk({ title, todolistId });
-  }, []);
-
-  const changeStatus = useCallback(function (taskId: string, status: TaskStatuses, todolistId: string) {
-    updateTask({ taskId, domainModel: { status }, todolistId });
-  }, []);
-
-  const changeTaskTitle = useCallback(function (taskId: string, title: string, todolistId: string) {
-    updateTask({ taskId, domainModel: { title }, todolistId });
-  }, []);
-
-  const changeFilter = useCallback(function (filter: FilterValuesType, id: string) {
-    changeTodolistFilter({ id, filter });
-  }, []);
-
-  const removeTodolist = useCallback(function (id: string) {
-    removeTodolistThunk(id);
-  }, []);
-
-  const changeTodolistTitle = useCallback(function (id: string, title: string) {
-    changeTodolistTitleThunk({ id, title });
-  }, []);
-
-  const addTodolist = useCallback((title: string) => {
-    addTodolistThunk(title);
-  }, []);
-
-  if (!isLoggedIn) {
-    return <Navigate to={"/login"} />;
-  }
-
-  return (
-    <>
-      <Grid container style={{ padding: "20px" }}>
-        <AddItemForm addItem={addTodolist} />
-      </Grid>
-      <Grid container spacing={3}>
-        {todolists.map((tl) => {
-          let allTodolistTasks = tasks[tl.id];
-
-          return (
-            <Grid item key={tl.id}>
-              <Paper style={{ padding: "10px" }}>
-                <Todolist
-                  todolist={tl}
-                  tasks={allTodolistTasks}
-                  removeTask={removeTask}
-                  changeFilter={changeFilter}
-                  addTask={addTask}
-                  changeTaskStatus={changeStatus}
-                  removeTodolist={removeTodolist}
-                  changeTaskTitle={changeTaskTitle}
-                  changeTodolistTitle={changeTodolistTitle}
-                />
-              </Paper>
+    return (
+        <>
+            <Grid container style={{padding: "20px"}}>
+                <AddItemForm addItem={addTodolist}/>
             </Grid>
-          );
-        })}
-      </Grid>
-    </>
-  );
+            <Grid container spacing={3}>
+                {todolists.map((tl) => {
+                    let allTodolistTasks = tasks[tl.id];
+
+                    return (
+                        <Grid item key={tl.id}>
+                            <Paper style={{padding: "10px"}}>
+                                <Todolist
+                                    todolist={tl}
+                                    tasks={allTodolistTasks}
+                                    changeFilter={changeFilter}
+                                    addTask={addTask}
+                                    removeTodolist={removeTodolist}
+                                    changeTodolistTitle={changeTodolistTitle}
+                                />
+                            </Paper>
+                        </Grid>
+                    );
+                })}
+            </Grid>
+        </>
+    );
 };
---
+++ src/features/todolists-list/todolists/ui/todolist/task/task.tsx
@@ -0,0 +1,48 @@
+import React, {ChangeEvent} from "react";
+import {Checkbox, IconButton} from "@mui/material";
+import {Delete} from "@mui/icons-material";
+import {EditableSpan} from "common/enums/components";
+import {TaskStatuses} from "common/enums";
+import {TaskType} from "features/todolists-list/tasks/api/tasks.api.types";
+import {useActions} from "common/hooks";
+import {tasksThunks} from "features/todolists-list/tasks/model/tasks.reducer";
+import s from './task.module.css'
+
+type Props = {
+    task: TaskType;
+    todolistId: string;
+};
+
+export const Task = React.memo(({task, todolistId}: Props) => {
+    const removeTaskHandler = () => removeTask({taskId: task.id, todolistId});
+
+    const {removeTask, updateTask} = useActions(tasksThunks)
+
+    const changeStatusHandler =
+        (e: ChangeEvent<HTMLInputElement>) => {
+            let newIsDoneValue = e.currentTarget.checked;
+            const status = newIsDoneValue ? TaskStatuses.Completed : TaskStatuses.New
+            updateTask({
+                taskId: task.id,
+                todolistId,
+                domainModel: {status}
+            })
+        };
+
+    const changeTitleHandler =
+        (title: string) => {
+            updateTask({taskId: task.id, todolistId, domainModel: {title}})
+        };
+
+    return (
+        <div key={task.id} className={task.status === TaskStatuses.Completed ? s.isDone : ""}>
+            <Checkbox checked={task.status === TaskStatuses.Completed} color="primary"
+                      onChange={changeStatusHandler}/>
+
+            <EditableSpan value={task.title} onChange={changeTitleHandler}/>
+            <IconButton onClick={removeTaskHandler}>
+                <Delete/>
+            </IconButton>
+        </div>
+    );
+});
