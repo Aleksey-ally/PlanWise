02e7f685c3b0eb3af97840677db02d4101a71467
Aleksey <easy.mail.to.copy@gmail.com>
2023-09-14 22:06:52 +0300

    Created and learned addMatcher, relocated logic for preloader into addMatcher

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -1,9 +1,9 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { AnyAction, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 const initialState = {
   status: "idle" as RequestStatusType,
   error: null as string | null,
-  isInitialized: false,
+  isInitialized: false
 };
 
 export type AppInitialStateType = typeof initialState;
@@ -21,8 +21,17 @@ const slice = createSlice({
     },
     setAppInitialized: (state, action: PayloadAction<{ isInitialized: boolean }>) => {
       state.isInitialized = action.payload.isInitialized;
-    },
+    }
   },
+  extraReducers: (builder) => {
+    builder.addMatcher(
+      (action: AnyAction) => {
+        return action.type.endsWith('/pending');
+      },
+      (state) => {
+        state.status = 'loading';
+      });
+  }
 });
 
 export const appReducer = slice.reducer;
